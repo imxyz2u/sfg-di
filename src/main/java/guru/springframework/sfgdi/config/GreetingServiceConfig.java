c04621dbf4269fb075d22c97d6e7e3e20b6eaee3
e753gh <Harry.Singh@ehi.com>
2021-08-16 00:10:26 -0500

    Converting annotation in to Java configuration

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,26 +1,51 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18NSpanishService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
 
   //this type of configuration is used for third party jars to bring the objects in to spring context
-  @Bean // adding bean annotation makes it a component
-  ConstructorGreetingService constructorGreetingService(){ // this gives a method to get the instance
+  @Bean
+  // adding bean annotation makes it a component
+  ConstructorGreetingService constructorGreetingService() { // this gives a method to get the instance
     return new ConstructorGreetingService();
   }
 
   @Bean
-  PropertyInjectedGreetingService propertyInjectedGreetingService(){
+  PropertyInjectedGreetingService propertyInjectedGreetingService() {
     return new PropertyInjectedGreetingService();
   }
 
-  @Bean SetterInjectedGreetingService setterInjectedGreetingService(){
+  @Bean
+  SetterInjectedGreetingService setterInjectedGreetingService() {
     return new SetterInjectedGreetingService();
+  }
+
+  @Profile("EN")
+  @Bean
+  I18nEnglishGreetingService i18nService() {
+    return new I18nEnglishGreetingService();
+  }
+
+  @Bean("i18nService")
+  @Profile("ES")
+  I18NSpanishService i18NSpanishService() {
+    return new I18NSpanishService();
+  }
+
+  @Bean
+  @Primary
+  PrimaryGreetingService primaryGreetingService() {
+    return new PrimaryGreetingService();
   }
 }
